94bb728047efb051e8b53840b424081022fcefd5
commento <riccardo@null.net>
2017-09-17 20:25:28 +0200

    update

---
+++ js/options.js
@@ -0,0 +1,82 @@
+var metro = new Audio("snd/metro.mp3");
+document.getElementById('metro').addEventListener('click', function(){
+     if (metro.paused) {
+       metro.play();
+       document.getElementById('metrotext').style.visibility = 'visible';
+       document.getElementById('metro').style.filter = 'brightness(50%)';
+     }
+});
+
+var gucci = new Audio("snd/gucci.mp3");
+document.getElementById('gucci').addEventListener('click', function(){
+     if (gucci.paused) {
+       gucci.play();
+       document.getElementById('guccitext').style.visibility = 'visible';
+       document.getElementById('gucci').style.filter = 'brightness(50%)';
+     }
+});
+
+
+var savage = new Audio("snd/21.mp3");
+document.getElementById('t1').addEventListener('click', function(){
+     if (savage.paused) {
+       savage.play();
+       document.getElementById('21text').style.visibility = 'visible';
+       document.getElementById('t1').style.filter = 'brightness(50%)';
+     }
+});
+
+var drake = new Audio("snd/drake.mp3");
+document.getElementById('drake').addEventListener('click', function(){
+     if (drake.paused) {
+       drake.play();
+       document.getElementById('draketext').style.visibility = 'visible';
+       document.getElementById('drake').style.filter = 'brightness(50%)';
+     }
+});
+
+
+var travis = new Audio("snd/travis.mp3");
+document.getElementById('travis').addEventListener('click', function(){
+     if (travis.paused) {
+       travis.play();
+       document.getElementById('travistext').style.visibility = 'visible';
+       document.getElementById('travis').style.filter = 'brightness(50%)';
+     }
+});
+
+var jahlil = new Audio("snd/jahlil.mp3");
+document.getElementById('jahlil').addEventListener('click', function(){
+     if (jahlil.paused) {
+       jahlil.play();
+       document.getElementById('jahliltext').style.visibility = 'visible';
+       document.getElementById('jahlil').style.filter = 'brightness(50%)';
+     }
+});
+
+var eoe = new Audio("snd/808.mp3");
+document.getElementById('808').addEventListener('click', function(){
+     if (eoe.paused) {
+       eoe.play();
+       document.getElementById('808text').style.visibility = 'visible';
+       document.getElementById('808').style.filter = 'brightness(50%)';
+     }
+});
+
+var migos = new Audio("snd/migos.mp3");
+document.getElementById('migos').addEventListener('click', function(){
+     if (migos.paused) {
+       migos.play();
+       document.getElementById('migostext').style.visibility = 'visible';
+       document.getElementById('migos').style.filter = 'brightness(50%)';
+     }
+});
+
+var mike = new Audio("snd/mike.mp3");
+document.getElementById('mike').addEventListener('click', function(){
+     if (mike.paused) {
+       mike.play();
+       document.getElementById('miketext').style.visibility = 'visible';
+       document.getElementById('mike').style.filter = 'brightness(50%)';
+     }
+});
